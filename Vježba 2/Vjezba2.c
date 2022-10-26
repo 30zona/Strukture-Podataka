8d16df64305f01f03aaf52ce6290368ba1a6e1a1
Borna <bgoret00@fesb.hr>
2022-10-27 01:14:04 +0200

    vjezba2

--- Vježba 2/Vjezba2.c
+++ Vježba 2/Vjezba2.c
@@ -12,79 +12,141 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define MAX_NAME (747)
+#define MAX_NAME (25)
 
-typedef struct osoba* pozicija;
-struct osoba{
+typedef struct osoba* pozicija;   
+
+typedef struct osoba{             //ovdi ga definiramo
  char ime[MAX_NAME];
  char prezime[MAX_NAME];
  int godina_rodjenja;
  pozicija next;
-};
+}osoba;
 
-void unosNaPocetakListe(pozicija o);
-void unosNaKrajListe(pozicija o);
-void ispisListe(pozicija o);
+void unosNaPocetakListe(pozicija);
+void unosNaKrajListe(pozicija);
+void ispisListe(pozicija);
+pozicija pronalaziElement(char*, pozicija);
+pozicija onajIza(char*, pozicija);
+osoba* stvaranje(void);
+int brisanjeElementa(pozicija);
 
-int main()
+int main()        //u mainu ih samo zovemo
 {
-    struct osoba o={0};
-    o.next=NULL;
-    unosNaPocetakListe(&o);
-    unosNaKrajListe(&o);
-    ispisListe(&o);
-    return 0;
-}
-
-void unosNaPocetakListe(pozicija o)
-{
-    pozicija q=NULL;
-    q=(pozicija)malloc(sizeof(struct osoba));
-    if(q==NULL) //provjeravamo je li malloc uspio naci prostora u memoriji
-    printf("malloc error");
-    else //nastavljamo ako je malloc uspio
-    {
-        printf("Unesite ime\n");
-        scanf(" %s",q->ime);
-        printf("Unesite prezime\n");
-        scanf(" %s",q->prezime);
-        printf("Unesite godinu rodjenja\n");
-        scanf("%d",&q->godina_rodjenja);
-        q->next=o->next;
-        o->next=q;
+    //struct osoba o={0};         //sve je dobro postavit na 0 il NULL
+    //o.next=NULL;
+    pozicija head = NULL;
+    head=stvaranje();
+    unosNaPocetakListe(head);
+    unosNaPocetakListe(head);
+    unosNaKrajListe(head);
+    ispisListe(head->next);
+    char str[25];
+    printf("Unesite trazeno prezime:\n");
+    scanf("%s",str);
+    pozicija x=NULL;
+    x=pronalaziElement(str, head->next);
+    if(x!=NULL){
+        printf("Trazena osoba:%s %s \n", x->ime,x->prezime);
     }
+    brisanjeElementa(head);
+    ispisListe(head->next);
+return 1;
 }
-void unosNaKrajListe(pozicija o)
+
+osoba* stvaranje()
 {
-    pozicija q=NULL;
-    q=(pozicija)malloc(sizeof(struct osoba));
-    if(q==NULL) //provjeravamo je li malloc uspio naci prostora u memoriji
-    printf("malloc error");
-    else //nastavljamo ako je malloc uspio
-    {
-        printf("Unesite ime\n");
-        scanf(" %s",q->ime);
-        printf("Unesite prezime\n");
-        scanf(" %s",q->prezime);
-        printf("Unesite godinu rodjenja\n");
-        scanf("%d",&q->godina_rodjenja);
-        q->next=NULL;
-        while(o->next!=NULL)
-        {
-            o=o->next;
-        }
-        o->next=q;
-    }
+    osoba* o = NULL;
+    o = (osoba*)malloc(sizeof(osoba));
+    o->next = NULL;
+    return o;
 }
+
 void ispisListe(pozicija o)
 {
-    o=o->next;//o samo pokazuje na prvi element, zato ga postavljamo na o->next, tj. prvi pravi element liste
-    if(o==NULL)
-    printf("Lista je prazna\n");
-    else while(o!=NULL)//idemo kroz listu sve dok ne dodemo do kraja
+    if(o==NULL){
+    printf("Lista je prazna\n");}
+
+    printf("Lista:\n");
+    while(o!=NULL)//idemo kroz listu sve dok ne dodemo do kraja
     {
-        printf("\nIme: %s %s\n", o->ime, o->prezime);
+        printf("Ime: %s %s\n", o->ime, o->prezime);
         printf("Godina rodjenja: %d\n", o->godina_rodjenja);
         o=o->next;//idemo na sljedeci element liste
     }
+}
+
+void unosNaPocetakListe(pozicija head)
+{ 
+    pozicija q=stvaranje();
+
+    if(q==NULL) //provjeravamo je li malloc uspio naci prostora u memoriji
+        printf("malloc error");
+    else //nastavljamo ako je malloc uspio
+    {
+        printf("Unesite ime\n");
+        scanf(" %s",q->ime);                   //sve stvari unosimo sa scanf, i spremamo ih na mjesto u strukturi q tipa pozicija
+        printf("Unesite prezime\n");
+        scanf(" %s",q->prezime);
+        printf("Unesite godinu rodjenja\n");
+        scanf("%d",&q->godina_rodjenja);       // za godinu koja je int treba ovaj &
+        q->next=head->next;
+        head->next=q;
+    }
+}
+void unosNaKrajListe(pozicija head)
+{
+pozicija q=head;
+    while(q->next != NULL)
+    {
+        q = q->next;
+    }
+    unosNaPocetakListe(q);
+}
+
+
+pozicija pronalaziElement(char* str, pozicija head)
+{
+    pozicija s= head;
+    while(s!=NULL && strcmp(s->prezime, str)!=0){
+        s=s->next;   
+    }
+    if (s==NULL){
+        printf("Osoba nije pronađena");
+    }
+    return s;
+}
+
+int brisanjeElementa(pozicija head)
+{
+    char str[20];
+    printf("Unesi prezime osobe za brisanje:");
+    scanf("%s", str);
+    
+    pozicija o = onajIza(str, head);
+    if(o == NULL)
+    {
+        printf("Ne postoji!");
+        return 0;
+    }
+
+    pozicija temp = o->next;                                        
+    o->next = temp->next;
+    free(temp);
+
+    return 1;
+}
+
+pozicija onajIza(char* str, pozicija head)
+{
+    pozicija s= head;
+    pozicija nova=head->next;
+    while(nova!=NULL && strcmp(nova->prezime, str)!=0){
+        s=nova;
+        nova=nova->next;
+    }
+    if (s==NULL){
+        printf("Osoba nije pronađena");
+    }
+    return s;
 }
